9781d1795cf273d9bc249accb42a7a6f991db092
Lucy M <lmcguigan@users.noreply.github.com>
2020-10-17 17:33:11 -0500

    2020 updates

--- assets/javascript/typescript.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function () {
-    var typed = new Typed("#typed", {
-        strings: ["great", "beautiful", "awesome", "snazzy", "really cool", "together!"],
-        typeSpeed: 100,
-        backSpeed: 100,
-    });
-});
